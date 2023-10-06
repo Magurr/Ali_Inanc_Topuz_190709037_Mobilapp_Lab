a60cc08062fd15ec35c0efa49d56fdfd1e82f80e
inanctopuz <inanctopuz@hotmail.com>
2023-10-06 22:59:13 +0300

    week1

---
+++ week1/app/src/main/java/aliinanctopuz/example/week1/MainActivity.java
@@ -0,0 +1,14 @@
+package aliinanctopuz.example.week1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
